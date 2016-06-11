0d8eac758edd82bb51047db4390d96412d0ede9d
VyacheslavDev <cokolok@gmail.com>
2016-06-12 01:44:43 +0300

    Complete class and programm

---
+++ ApplicationForControl/ControlPanel.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace ApplicationForControl
+{
+    partial class ControlPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControlPanel));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.VisiblePressOnLableName);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name task";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(476, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.InsertPathInTextBox);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Path to the file";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(305, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Start Program";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Stop Program";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(406, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = ":";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(424, 170);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(53, 22);
+            this.numericUpDown2.TabIndex = 19;
+            this.numericUpDown2.ThousandsSeparator = true;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(347, 170);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(53, 22);
+            this.numericUpDown4.TabIndex = 18;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(165, 170);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(165, 22);
+            this.dateTimePicker2.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(406, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(12, 17);
+            this.label7.TabIndex = 24;
+            this.label7.Text = ":";
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(424, 115);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(53, 22);
+            this.numericUpDown5.TabIndex = 23;
+            this.numericUpDown5.ThousandsSeparator = true;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(347, 115);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(53, 22);
+            this.numericUpDown6.TabIndex = 22;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(165, 115);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(165, 22);
+            this.dateTimePicker3.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 40);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Add task to XML file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 40);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Clean XML file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.CleanXMLfile);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label5.Location = new System.Drawing.Point(231, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 36);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Status";
+            this.label5.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(499, 28);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "Open Directory with the files";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OpenMainDir);
+            // 
+            // ControlPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(525, 323);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ControlPanel";
+            this.Text = "Control Panel for create XML";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ControlPanel_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ApplicationForControl/ControlPanel.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ApplicationForControl
+{
+    public partial class ControlPanel : Form
+    {
+        /// <summary>
+        /// Путь к корневой папке
+        /// </summary>
+        private static string PathToMyDoc = System.AppDomain.CurrentDomain.BaseDirectory + @"\";
+
+        public ControlPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text == "" || textBox1.Text == "")
+                {
+                    Fail();
+                }
+                else
+                {
+                    CreateXMLBasic();
+                    AddTaskToXml();
+                    OK();
+                }
+
+            }
+            catch { FatalError(); }
+        }
+        /// <summary>
+        /// Создает надпись в статусе "ОК"
+        /// </summary>
+        private void OK()
+        {
+            label5.Text = "OK";
+            label5.ForeColor = Color.DarkGreen;
+            label5.Visible = true;
+
+        }
+        /// <summary>
+        /// Создает надпись в статусе "Fail"
+        /// </summary>
+        private void Fail()
+        {
+            label5.Text = "Fail";
+            label5.ForeColor = Color.Red;
+            label5.Visible = true;
+        }
+        /// <summary>
+        /// Создает надпись в статусе "FatalError"
+        /// </summary>
+        private void FatalError()
+        {
+            label5.Text = "FatalError";
+            label5.ForeColor = Color.Blue;
+            label5.Visible = true;
+        }
+        /// <summary>
+        /// Открывает проводник для того чтобы узнать путь к exe-файлу
+        /// </summary>
+        /// <returns>Путь к exeшнику</returns>
+        private string FilePath()
+        {
+            using (OpenFileDialog Ofd = new OpenFileDialog())
+            {
+                if(Ofd.ShowDialog() == DialogResult.OK)
+                {
+                    return Ofd.FileName;
+                }
+
+            }
+            return null;
+        }
+        /// <summary>
+        /// Скрывает статус когда происходит заполнения окна данными
+        /// </summary>
+        private void VisiblePressOnLableName(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+        }
+        /// <summary>
+        /// По нажатию на кнопку очищения, происходит удаление XML файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CleanXMLfile(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            try
+            {
+                if (File.Exists(PathToMyDoc + "Settings.xml"))
+                {
+                    File.Delete(PathToMyDoc + "Settings.xml");
+                    OK();
+                }
+                else { Fail(); }
+            }
+            catch { FatalError(); }
+           
+            
+        }
+        /// <summary>
+        /// Создает новую задачу в xml файл
+        /// </summary>
+        private void AddTaskToXml()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(PathToMyDoc + "Settings.xml");
+
+            XmlElement name = xmlDoc.CreateElement("Name");
+            name.InnerText = textBox1.Text;
+            xmlDoc.DocumentElement.AppendChild(name);
+
+            XmlElement path = xmlDoc.CreateElement("path");
+            path.InnerText = textBox2.Text;
+            xmlDoc.DocumentElement.AppendChild(path);
+
+            XmlElement startTime = xmlDoc.CreateElement("StartTime");
+            startTime.InnerText = string.Format("{0:dd.MM.yyyy} {1}:{2}", dateTimePicker3.Value, numericUpDown6.Value, numericUpDown5.Value);
+            xmlDoc.DocumentElement.AppendChild(startTime);
+
+            XmlElement stopTime = xmlDoc.CreateElement("StopTime");
+            stopTime.InnerText = string.Format("{0:dd.MM.yyyy} {1}:{2}", dateTimePicker2.Value, numericUpDown4.Value, numericUpDown2.Value);
+            xmlDoc.DocumentElement.AppendChild(stopTime);
+
+            xmlDoc.Save(PathToMyDoc + "Settings.xml");
+        }
+        /// <summary>
+        /// Задает путь в текст бокс
+        /// </summary>
+        private void InsertPathInTextBox(object sender, EventArgs e)
+        {
+            textBox2.Text = FilePath();
+        }
+        /// <summary>
+        /// Создает XML файл и создает первоначальную DOM структуру.
+        /// </summary>
+        private void CreateXMLBasic()
+        {
+            if (!File.Exists(PathToMyDoc + "Settings.xml"))
+            {
+
+                XmlTextWriter textWritter = new XmlTextWriter(PathToMyDoc + "Settings.xml", Encoding.UTF8);
+                textWritter.WriteStartDocument();
+                textWritter.WriteStartElement("Task");
+                textWritter.WriteEndElement();
+                textWritter.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+        }
+        
+        private void ControlPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            label5.Visible = false;
+        }
+        /// <summary>
+        /// Открытие корневой директории по нажатию на кнопку
+        /// </summary>
+        private void OpenMainDir(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer", System.AppDomain.CurrentDomain.BaseDirectory);
+        }
+    }
+}
